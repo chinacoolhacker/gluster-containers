8d993d063d07e22c1d272e8163d3efbbb565ae45
chinacoolhacker <chinacoolhacker@gmail.com>
2018-02-27 16:20:45 +0300

    gluster-geo-setup

--- CentOS/gluster-geo-setup.sh
+++ CentOS/gluster-geo-setup.sh
@@ -3,19 +3,6 @@
 geosetup(){
   touch /var/lib/glusterd/geo-replication/authorized_keys
   chmod 600 /var/lib/glusterd/geo-replication/authorized_keys
-
-# wait glusterd 
-  sleep 30
-
-  if ! test -f /var/lib/glusterd/geo-replication/secret.pem ; then
-      gluster-georep-sshkey generate --no-prefix
-  fi
-
-
-  if ! test -f /var/lib/glusterd/geo-replication/common_secret.pem.pub ; then
-      gluster-georep-sshkey generate --no-prefix
-  fi
-
   sed 's#/nonexistent/gsyncd#/usr/libexec/glusterfs/gsyncd#g' -i /var/lib/glusterd/geo-replication/gsyncd_template.conf; \
 
   echo "Script Ran Successfully"
